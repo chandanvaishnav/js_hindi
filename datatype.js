5ab5542ac87d38880b6eb8841d1c4b34e5d03ee6
chandan vaishnav <chandanvishnav98@gmail.com>
2024-12-12 17:04:42 +0530

    chandanvaishnav

---
+++ datatype.js
@@ -0,0 +1,35 @@
+"ue strict";// means he treate the code as new version 
+
+//alert(3+3)   // we use node js not a browser
+
+//console.log(3+3) console.log("hii")// in this errror show not allowed
+
+//console.log(3+3);console.log("hii")//it is allowed  but it is bas for reading and not use too good coder but its run also
+
+console.log(3+3)
+
+console.log("hii")
+/*  its is agod practise to god coder */
+
+
+// type of data type
+/* 
+1 number
+2 bigint
+3 string
+4 boolean
+5 null--it is stand alon value
+6 undefined - its temproery not defing
+7 symbol-unique
+8 object
+
+*/
+
+let name="chandan"
+let age=32
+let islogin=true
+
+
+console.log(typeof age)// type samjto 
+console.log(typeof null)
+console.log(typeof undefined)
---
+++ text.js
@@ -0,0 +1,15 @@
+const accountid = 32456
+var accountemil = "chandan@gmil.com"
+let accountpassword = "2134"
+accountcity = "jaypur"
+let accountsatate //;he apya var marjivar ahe lihine ki nahi
+//accountid=4// not allowed
+accountemil = "vaishnav@gmil.com"
+accountpassword = "233223232"
+accountcity = "nashik"
+console.log(accountid);
+
+/*
+not  use var becue issues in functional and block and scpe
+*/
+console.table([accountid, accountemil, accountpassword, accountcity,accountsatate])
