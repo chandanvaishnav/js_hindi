d985373f3375ffaf68906dbffef806bb8910373e
chandan vaishnav <chandanvishnav98@gmail.com>
2024-12-13 14:27:26 +0530

    comparision

---
+++ comparision.js
@@ -0,0 +1,21 @@
+/*console.log(2>1)
+consolr.log(2>1)
+consolr.log(2>1)
+consolr.log(2>1)
+consolr.log(2>1)
+consolr.log(2>1)*/
+
+//console.log("2" > 1);
+//console.log(2 > "3")
+
+//console.log(null>0)
+//console.log(null==0)
+//console.log(null>=0)
+
+//console.log(undefined>0)
+//console.log(undefined==0)
+//console.log(undefined>=0)
+
+// === -> strict check
+//console.log("2"==2)
+//console.log("2"===2) they check data type also 
