eaa1ff5a21a5d50785fa343bb772cd4ef12b5b9b
chandan vaishnav <chandanvishnav98@gmail.com>
2024-12-13 13:10:54 +0530

    dataconvrsionopretion

--- conversionopertion.js
+++ conversionopertion.js
@@ -1,74 +1,125 @@
-/*let score=100
+let score = 100
 
-console.log(typeof score)
-console.log(typeof (score));
+//console.log(typeof score)
+//console.log(typeof (score));
 
-score="100"
-console.log(typeof score)
+score = "100"
+//console.log(typeof score)
 
-let vinnumber=Number(score)
-console.log(typeof score)
-console.log(typeof vinnumber)
-console.log(vinnumber)
+let vinnumber = Number(score)
+//console.log(typeof score)
+//console.log(typeof vinnumber)
+//console.log(vinnumber)
 
-score="100shatak"
-vinnumber=Number(score)
-console.log(typeof vinnumber)
-console.log(typeof score)
-console.log(vinnumber)// output is non menas not a number
-
-
-score="shatak"
-vinnumber=Number(score)
-console.log(typeof vinnumber)
-console.log(typeof score)
-console.log(vinnumber)// also output not anumber
-
-score=null
-vinnumber=Number(score)
-console.log(typeof vinnumber)
-console.log(typeof score)
-console.log(vinnumber)// also output 0
-
-score=undefined
-vinnumber=Number(score)
-console.log(typeof vinnumber)
-console.log(typeof score)
-console.log(vinnumber)// also output not anumber
+score = "100shatak"
+vinnumber = Number(score)
+//console.log(typeof vinnumber)
+//console.log(typeof score)
+//console.log(vinnumber)// output is non menas not a number
 
 
-score=true
-vinnumber=Number(score)
-console.log(typeof vinnumber)
-console.log(typeof score)
-console.log(vinnumber)// also output 1
-*/
+score = "shatak"
+vinnumber = Number(score)
+//console.log(typeof vinnumber)
+//console.log(typeof score)
+//console.log(vinnumber)// also output not anumber
 
-/*let islogedin=0
-let binlogedin=Boolean(islogedin)
-console.log(binlogedin)
+score = null
+vinnumber = Number(score)
+//console.log(typeof vinnumber)
+//console.log(typeof score)
+//console.log(vinnumber)// also output 0
 
-islogedin=1
-binlogedin=Boolean(islogedin)
-console.log(binlogedin)
+score = undefined
+vinnumber = Number(score)
+//console.log(typeof vinnumber)
+//console.log(typeof score)
+//console.log(vinnumber)// also output not anumber
 
-islogedin=""
-binlogedin=Boolean(islogedin)
-console.log(binlogedin)
 
-islogedin="vaibhav"
-binlogedin=Boolean(islogedin)
-console.log(binlogedin)
+score = true
+vinnumber = Number(score)
+//console.log(typeof vinnumber)
+//console.log(typeof score)
+//console.log(vinnumber)// also output 1
 
-islogedin="5"
-binlogedin=Boolean(islogedin)
-console.log(binlogedin)
 
-islogedin="5.0"
-binlogedin=Boolean(islogedin)
-console.log(binlogedin)*/
+let islogedin = 0
+let binlogedin = Boolean(islogedin)
+//console.log(binlogedin)
+
+islogedin = 1
+binlogedin = Boolean(islogedin)
+//console.log(binlogedin)
+
+islogedin = ""
+binlogedin = Boolean(islogedin)
+//console.log(binlogedin)
+
+islogedin = "vaibhav"
+binlogedin = Boolean(islogedin)
+//console.log(binlogedin)
+
+islogedin = "5"
+binlogedin = Boolean(islogedin)
+//console.log(binlogedin)
+
+islogedin = "5.0"
+binlogedin = Boolean(islogedin)
+//console.log(binlogedin)
 
 let number = 23
 let stringconversion = String(number)
-console.log(stringconversion)
-console.log(typeof stringconversion)
+//console.log(stringconversion)
+//console.log(typeof stringconversion)
+
+
+//**********************************************opretion********************************************************** */
+
+
+
+let value = 20
+let negativevalue = -value
+//console.log(negativevalue)
+
+value = -20
+negativevalue = -value
+//console.log(negativevalue)
+
+value = -20
+negativevalue = value
+//console.log(negativevalue)
+
+
+
+/*onsole.log(2 + 2)
+console.log(2 - 2)
+console.log(2 * 2)
+console.log(2 / 2)
+console.log(2 ** 2)
+console.log(2 % 2)
+*/
+
+let str1 = "chandan"
+let str2 = "vaishnav"
+let str3 = str2 + str1
+//console.log(str3)
+//let str3=str1+str2
+//console.log(str3)
+
+//console.log(str1+str2)
+
+//console.log("true")
+//console.log(true)
+//console.log(+true)
+//console.log(true+)    errrorr oaccure not allowed bad
+
+
+//console.log("false")
+//console.log(false)
+//console.log(+false)
+
+//console.log(+"")
+
+//   prefix or postfix sikneke liye link per clik karo 
+//https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Increment#try_it
