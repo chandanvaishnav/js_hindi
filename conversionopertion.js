aba84c1d8d96ed2ce42c2835c0a7747f9c6fc9ae
chandan vaishnav <chandanvishnav98@gmail.com>
2024-12-13 11:52:18 +0530

    conversion

---
+++ conversionopertion.js
@@ -0,0 +1,74 @@
+/*let score=100
+
+console.log(typeof score)
+console.log(typeof (score));
+
+score="100"
+console.log(typeof score)
+
+let vinnumber=Number(score)
+console.log(typeof score)
+console.log(typeof vinnumber)
+console.log(vinnumber)
+
+score="100shatak"
+vinnumber=Number(score)
+console.log(typeof vinnumber)
+console.log(typeof score)
+console.log(vinnumber)// output is non menas not a number
+
+
+score="shatak"
+vinnumber=Number(score)
+console.log(typeof vinnumber)
+console.log(typeof score)
+console.log(vinnumber)// also output not anumber
+
+score=null
+vinnumber=Number(score)
+console.log(typeof vinnumber)
+console.log(typeof score)
+console.log(vinnumber)// also output 0
+
+score=undefined
+vinnumber=Number(score)
+console.log(typeof vinnumber)
+console.log(typeof score)
+console.log(vinnumber)// also output not anumber
+
+
+score=true
+vinnumber=Number(score)
+console.log(typeof vinnumber)
+console.log(typeof score)
+console.log(vinnumber)// also output 1
+*/
+
+/*let islogedin=0
+let binlogedin=Boolean(islogedin)
+console.log(binlogedin)
+
+islogedin=1
+binlogedin=Boolean(islogedin)
+console.log(binlogedin)
+
+islogedin=""
+binlogedin=Boolean(islogedin)
+console.log(binlogedin)
+
+islogedin="vaibhav"
+binlogedin=Boolean(islogedin)
+console.log(binlogedin)
+
+islogedin="5"
+binlogedin=Boolean(islogedin)
+console.log(binlogedin)
+
+islogedin="5.0"
+binlogedin=Boolean(islogedin)
+console.log(binlogedin)*/
+
+let number = 23
+let stringconversion = String(number)
+console.log(stringconversion)
+console.log(typeof stringconversion)
