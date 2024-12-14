c5a30dfdf767c998ce8451d827c0b9976925a727
chandan vaishnav <chandanvishnav98@gmail.com>
2024-12-14 16:13:45 +0530

    string

---
+++ numandmaths.js
@@ -0,0 +1,33 @@
+const score = 400
+//console.log(score)
+const balaance = new Number(100)
+//console.log(balaance)
+//console.log(typeof balaance)
+//console.log(balaance.toString().length)
+//console.log(typeof balaance)
+//console.log(balaance.toFixed(4))
+
+const value = 10000000000
+//console.log(value.toLocaleString('en-IN'))
+
+
+//**************************maths******************************************** */
+
+
+//console.log(Math)
+//console.log(Math.abs(-4))
+//console.log(Math.round(4.9))
+//console.log(Math.ceil(4.9))
+//console.log(Math.floor(4.9))
+//console.log(Math.min(4,7,5,87,9))
+//console.log(Math.max(4,7,5,87,9))
+
+
+console.log(Math.random())
+console.log((Math.random() * 10) + 1)
+console.log(Math.floor(Math.random() * 10) + 1)
+
+const min = 100
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1)+min))
