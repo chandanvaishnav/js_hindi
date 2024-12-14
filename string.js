ddc4d079bf6904ce0196a2e9f0b76c71b2fb6034
chandan vaishnav <chandanvishnav98@gmail.com>
2024-12-14 14:36:37 +0530

    string

---
+++ string.js
@@ -0,0 +1,32 @@
+let nav = "chandan"
+let repocount = 1
+//console.log(nav + repocount)*****this is a old fasion or not for pro devolopers
+//console.log(`my name is ${nav} and my repo count is ${repocount}`);
+//it is a ggod devlopers
+
+const gajitname = new String('chan-dan-vaish-nav')
+//console.log(gajitname)
+//console.log(gajitname[0])
+//console.log(gajitname.length)
+//console.log(gajitname.toLocaleLowerCase())
+//console.log(gajitname.toUpperCase())
+//console.log(gajitname.charAt(4))
+//console.log(gajitname.indexOf('i'))
+
+//const yantra=gajitname.substring(0,7)
+//console.log(yantra)
+
+//const other=gajitname.slice(-15,7)
+//console.log(gajitname)
+//console.log(other)
+
+
+const newstring = "                                              jhbvvbdvbmh                                             "
+//console.log(newstring)
+//console.log(newstring.trim())
+
+const url = "https:///chandan/%200000000000000baisjhnav"
+console.log(url.replace('%200000000000000', '545454545454'))
+console.log(url.includes('hitesh'))
+
+console.log(gajitname.split('-'))
