87abe1dc5d22749b8a3b4b6f33b3cf8223857814
chandan vaishnav <chandanvishnav98@gmail.com>
2024-12-17 15:47:38 +0530

    basics

---
+++ 02_basics/aray.js
@@ -0,0 +1,51 @@
+
+const my_arry = [1, 2, 3, 45, , 67, 4, true,]
+const myHeroes = ["viratkohali", "rohitsharma", "msdhini", "sachintendulkar", "jaspritbumra"]
+const myArr2 = new Array(1, 2, 3, 4, 5, 5)
+//console.log(myHeroes[0])
+//console.log(my_arry[3])
+//console.log(myArr2[4])
+
+
+//array methods
+
+myArr2.push(10)
+//console.log(myArr2)
+myArr2.push(543554)
+//console.log(myArr2)
+myArr2.pop()
+//console.log(myArr2)
+
+myArr2.unshift(8)
+//console.log(myArr2)
+myArr2.shift()
+//console.log(myArr2)
+
+//console.log(myArr2.includes(21))
+//console.log(myArr2.indexOf(21))
+//console.log(myArr2.indexOf(2))
+
+
+
+const newaray = myArr2.join()
+//console.log(newaray)
+//console.log(typeof newaray)
+
+
+//console.log(myArr2)
+//console.log(typeof myArr2)
+
+//console.log(myArr2)
+
+//slice and splice
+
+console.log(1, myArr2)
+let araynew = myArr2.slice(1, 3)
+console.log(araynew)
+console.log(myArr2)
+
+console.log("splice")
+console.log(1, myArr2)
+araynew = myArr2.splice(1, 3)
+console.log(araynew)
+console.log(myArr2)
